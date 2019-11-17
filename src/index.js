a3cf46818151069fc7cad4bdc1ba1b32643cfd67
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-17 18:55:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import ToggleButton from './components/ToggleButton';
 
 ReactDOM.render(
+  <ToggleButton />,
   <div>Components Need To Be Mounted</div>,
   document.getElementById('root')
 )
